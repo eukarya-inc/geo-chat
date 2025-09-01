864dc3bcf18174d4feccd5391005179e437bacd3
rot1024 <aayhrot@gmail.com>
2025-09-01 16:34:15 +0900

    chore: change base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/duckdb-wasm-prototype/',
+    base: '/links-bi-prototype/',
     plugins: [react(), tailwindcss()],
     optimizeDeps: {
         exclude: ["@duckdb/duckdb-wasm"],
